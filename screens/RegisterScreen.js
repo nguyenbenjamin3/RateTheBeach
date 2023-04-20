d6672f6ccc046e490193586a75c3d13a8f6a4184
Daryl Nguyen <86028937+nguyendaryl@users.noreply.github.com>
2023-04-20 01:43:59 -0700

    minor change to registerScreen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,22 +1,24 @@
 //will eventually come from login screen, won't actually be part of navbar
 
-import {StyleSheet, Text, View} from 'react-native';
+import {KeyboardAvoidingView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import { TextInput } from 'react-native-gesture-handler';
 
 const RegisterScreen = () => {
   return (
-    <View>
-      <TextInput
-      placeholder = "Elbee.Shark@csulb.edu"
-      ></TextInput>
+    <KeyboardAvoidingView>
+      <View>
+        <TextInput
+        placeholder = "Elbee.Shark@csulb.edu"
+        ></TextInput>
         <TextInput
         placeholder = "Enter a password"
         ></TextInput>
         <TextInput
         placeholder = "Re-enter password"
         ></TextInput>
-    </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
