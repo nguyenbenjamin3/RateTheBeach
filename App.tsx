df680db99a83fb837a9c6aa33a13c4810db88b29
ksu4 <kennethsu0@gmail.com>
2023-04-20 01:34:34 -0700

    fixed more navv bar stuff
    added icons, need to remove header on it, need to fix other things on it.

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import Tabs  from './navigation/tabs';
 import DisplayScreens from './navigation/DisplayScreens';
 
@@ -19,6 +20,7 @@ export default function App() {
           <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
         </Stack.Navigator> }
 
       </NavigationContainer>
--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,8 +1,9 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View,TouchableOpacity,Image} from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -23,9 +24,52 @@ const Tabs = () => {
             }
          }}
 >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Login" component={LoginScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options = {{
+        tabBarIcon: ({focused}) => (
+            <View 
+            style={{
+                alignItems:"center", justifyContent: "center",top: 2,
+            }}> 
+                <Image source={require('../images/icon_home_.png')}
+                resizeMode = 'contain' 
+                style={{
+                    width: 20, height: 20
+                }}/>
+        
+                    <Text style={{color: focused ? "#ffcd89" : "#748c94",fontSize: 12,}}>HOME
+                      </Text>
+                       </View>),}}/>
+      <Tab.Screen name="Login" component={LoginScreen} options = {{
+        tabBarIcon: ({focused}) => (
+            <View 
+            style={{
+                alignItems:"center", justifyContent: "center",top: 2,
+            }}>
+                <Image source={require('../images/icon_heart_.png')}
+                resizeMode = 'contain' 
+                style={{
+                    width: 20, height: 20
+                }}/>
+        
+                    <Text style={{color: focused ? "#ffcd89" : "#748c94",fontSize: 12,}}>LOGIN
+                      </Text>
+                       </View>),}}/>
+      <Tab.Screen name="Profile" component={ProfileScreen} options = {{
+        tabBarIcon: ({focused}) => (
+            <View 
+            style={{
+                alignItems:"center", justifyContent: "center",top: 2,
+            }}>
+                
+                <Image source={require('../images/icon_person_.png')}
+                resizeMode = 'contain' 
+                style={{
+                    width: 20, height: 20
+                }}/>
+        
+                    <Text style={{color: focused ? "#ffcd89" : "#748c94",fontSize: 12,}}>PROFILE
+                      </Text>
+                       </View>),}}/>
 
     </Tab.Navigator>
   );
