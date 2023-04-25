48eb3e212b6052295817cb6e8c994f6238d1cd02
James Ta <james.ta01@student.csulb.edu>
2023-04-25 16:52:17 -0700

    SearchScreen1
    Searchbar that you can put text in

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,14 +1,57 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, TextInput, View, Text, ScrollView, Image, TouchableOpacity} from "react-native";
 import React from 'react';
+import {collection, getDocs, orderBy, query} from 'firebase/firestore';
 
 const SearchScreen = () => {
+
+  const data = async () =>{};//Gets all posts made
+  
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    	//When Search is clicked on the Navigation Bar, display this
+      <ScrollView>
+        <View style = {styles.container}>
+
+          <View>
+            <TextInput
+            style={{
+              height: 50,
+              width: 355,
+              fontSize: 20,
+              borderColor: 'gray',
+              borderWidth: 2,
+            }}
+            defaultValue = "Search for posts..."
+        ></TextInput>
+          </View>
+
+          <Image
+            source={require('../images/search.png')}
+            style={{
+              marginLeft: 5,
+              width: 20,
+              height: 20,
+            }}
+          />
+        </View>
+
+        <TouchableOpacity>
+        </TouchableOpacity>
+
+      </ScrollView>
+
   );
 };
 
 export default SearchScreen;
 
-const styles = StyleSheet.create({});
+// styles
+const styles = StyleSheet.create({
+  container: {
+    margin: 5,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "90%",
+
+  }
+});
