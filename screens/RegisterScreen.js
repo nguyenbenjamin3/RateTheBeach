b66127760fdfedfc55d6c7c7c8d6bc6cc54121cf
Daryl Nguyen <86028937+nguyendaryl@users.noreply.github.com>
2023-04-24 21:05:42 -0700

    extended register backend

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -3,8 +3,25 @@
 import {KeyboardAvoidingView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import { TextInput } from 'react-native-gesture-handler';
+import {auth} from '../firebase'
+import { Button } from 'react-native-paper';
+import {useState} from 'react'
+import { black } from 'react-native-paper/lib/typescript/src/styles/themes/v2/colors';
+
 
 const RegisterScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleRegister = () => {
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(userCredentials => {
+      const user = userCredentials.user;
+      console.log(user.email);
+    })
+    .catch(error => alert(error.message))
+  } 
   return (
     <KeyboardAvoidingView>
       <View>
@@ -17,6 +34,14 @@ const RegisterScreen = () => {
         <TextInput
         placeholder = "Re-enter password"
         ></TextInput>
+        <Button
+          onPress = {handleRegister}
+          title = "Register"
+          textColor = "black"
+          
+          
+
+        />
       </View>
     </KeyboardAvoidingView>
   );
