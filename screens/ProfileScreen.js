5188eabe2f6038bbe887e98f48b270628a1bd077
Benjamin Nguyen <nguyenbenjamin3@gmail.com>
2023-05-02 11:31:25 -0700

    profile screen changes

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,34 +1,56 @@
-import React from 'react';
-import {useNavigation} from '@react-navigation/core';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import OnboardingScreen from './OnboardingScreen';
-import { auth } from '../firebase';
+import {auth, db} from '../firebase';
+import {collection, getDocs} from 'firebase/firestore';
 
 const ProfileScreen = ({navigation}) => {
+  const [userFirstName, setUserFirstName] = useState('');
+  const [userLastName, setUserLastName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
 
+  // Fetch user data when component mounts
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const usersRef = collection(db, 'users');
+        const querySnapshot = await getDocs(usersRef);
+        querySnapshot.forEach(doc => {
+          const userData = doc.data();
+          if (userData.uid === auth.currentUser.uid) {
+            setUserFirstName(userData.firstName);
+            setUserLastName(userData.lastName);
+            setUserEmail(userData.email);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-const handleSignOut = () => {
-  auth
-  .signOut()
-  .then(() => {
-    navigation.navigate('Onboarding')
+    fetchUser();
+  }, [auth.currentUser.uid]);
 
-  })
-  .catch(error => alert(error.message))
-}
-
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate('Onboarding');
+      })
+      .catch(error => alert(error.message));
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>ProfileScreen</Text>
+      <Text
+        style={
+          styles.nameText
+        }>{`First Name: ${userFirstName} \nLast Name: ${userLastName} \nEmail: ${userEmail}`}</Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} 
-        onPress = {handleSignOut}
-        >
+        <TouchableOpacity style={styles.button} onPress={handleSignOut}>
           <Text style={styles.buttonText}>Sign Out</Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
 };
@@ -63,5 +85,9 @@ const styles = StyleSheet.create({
     color: 'black',
     fontWeight: '700',
     fontSize: 16,
+  },
+  nameText: {
+    fontSize: 16,
+    marginBottom: 10,
   },
 });
